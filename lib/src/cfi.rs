d814b1aef87c4156f9ae1c067e1556155002c8b7
Carl Lundin <clundin@google.com>
2026-02-20 16:08:02 -0800

    Sync CFI with Caliptra-SW commit 8b7dfabe
    This library is a out of date and we'll need to sync before we can use
    it.

--- lib/src/cfi.rs
+++ lib/src/cfi.rs
@@ -14,18 +14,15 @@ References:
     https://github.com/lowRISC/opentitan/blob/7a61300cf7c409fa68fd892942c1d7b58a7cd4c0/sw/device/lib/base/hardened.h#L260
 
 --*/
+
 use crate::CfiCounter;
 use core::cfg;
 use core::cmp::{Eq, Ord, PartialEq, PartialOrd};
-use core::marker::Copy;
+use core::marker::{Copy, PhantomData};
 
 /// CFI Panic Information
-#[repr(C)]
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum CfiPanicInfo {
-    /// Unknown error
-    UnknownError,
-
     /// CFI Counter decode error
     CounterCorrupt,
 
@@ -61,7 +58,11 @@ pub enum CfiPanicInfo {
 
     /// An enum match statement finds an unexpected value.
     UnexpectedMatchBranch,
+
+    /// Unknown error
+    UnknownError,
 }
+
 
 /// Launder the value to prevent compiler optimization
 ///
@@ -72,15 +73,106 @@ pub enum CfiPanicInfo {
 /// # Returns
 ///
 /// `T` - Same value
-pub fn cfi_launder<T>(val: T) -> T {
+pub fn cfi_launder<T>(val: T) -> T
+where
+    Launder<T>: LaunderTrait<T>,
+{
     if cfg!(feature = "cfi") {
-        // Note: The black box seems to be disabling more optimization
-        // than necessary and results in larger binary size
-        core::hint::black_box(val)
+        Launder { _val: PhantomData }.launder(val)
     } else {
         val
     }
 }
+
+pub trait LaunderTrait<T> {
+    fn launder(&self, val: T) -> T {
+        core::hint::black_box(val)
+    }
+}
+
+pub struct Launder<T> {
+    _val: PhantomData<T>,
+}
+
+// Inline-assembly laundering trick is adapted from OpenTitan:
+// https://github.com/lowRISC/opentitan/blob/master/sw/device/lib/base/hardened.h#L193
+//
+// NOTE: This implementation is LLVM-specific, and should be considered to be
+// a no-op in every other compiler. For example, GCC has in the past peered
+// into the insides of assembly blocks.
+//
+// At the time of writing, it seems preferable to have something we know is
+// correct rather than being overly clever; this is recorded here in case
+// the current implementation is unsuitable and we need something more
+// carefully tuned.
+//
+// Unlike in C, we don't have volatile assembly blocks, so this doesn't
+// necessarily prevent reordering by LLVM.
+//
+// When we're building for static analysis, reduce false positives by
+// short-circuiting the inline assembly block.
+impl LaunderTrait<u32> for Launder<u32> {
+    #[allow(asm_sub_register)]
+    fn launder(&self, val: u32) -> u32 {
+        let mut val = val;
+        // Safety: this is a no-op, since we don't modify the input.
+        unsafe {
+            // We use inout so that LLVM thinks the value might
+            // be mutated by the assembly and can't eliminate it.
+            core::arch::asm!(
+                "/* {t} */",
+                t = inout(reg) val,
+            );
+        }
+        val
+    }
+}
+
+impl LaunderTrait<bool> for Launder<bool> {
+    #[allow(asm_sub_register)]
+    fn launder(&self, val: bool) -> bool {
+        let mut val = val as u32;
+        // Safety: this is a no-op, since we don't modify the input.
+        unsafe {
+            core::arch::asm!(
+                "/* {t} */",
+                t = inout(reg) val,
+            );
+        }
+        val != 0
+    }
+}
+
+impl LaunderTrait<usize> for Launder<usize> {
+    #[allow(asm_sub_register)]
+    fn launder(&self, mut val: usize) -> usize {
+        // Safety: this is a no-op, since we don't modify the input.
+        unsafe {
+            core::arch::asm!(
+                "/* {t} */",
+                t = inout(reg) val,
+            );
+        }
+        val
+    }
+}
+
+impl<const N: usize, T> LaunderTrait<[T; N]> for Launder<[T; N]> {}
+impl<'a, const N: usize, T> LaunderTrait<&'a [T; N]> for Launder<&'a [T; N]> {
+    fn launder(&self, val: &'a [T; N]) -> &'a [T; N] {
+        let mut valp = val.as_ptr() as *const [T; N];
+        // Safety: this is a no-op, since we don't modify the input.
+        unsafe {
+            core::arch::asm!(
+                "/* {t} */",
+                t = inout(reg) valp,
+            );
+            &*valp
+        }
+    }
+}
+impl LaunderTrait<Option<u32>> for Launder<Option<u32>> {}
+impl LaunderTrait<CfiPanicInfo> for Launder<CfiPanicInfo> {}
 
 /// Control flow integrity panic
 ///
@@ -108,10 +200,10 @@ pub fn cfi_panic(info: CfiPanicInfo) -> ! {
         #[cfg(not(feature = "cfi-test"))]
         {
             extern "C" {
-                fn cfi_panic_handler(info: CfiPanicInfo) -> !;
+                fn cfi_panic_handler(code: u32) -> !;
             }
             unsafe {
-                cfi_panic_handler(info);
+                cfi_panic_handler(u32::from(info).into());
             }
         }
     }
@@ -135,6 +227,7 @@ macro_rules! cfi_assert_macro {
         pub fn $name<T>(lhs: T, rhs: T)
         where
             T: $trait1 + $trait2,
+            Launder<T>: LaunderTrait<T>,
         {
             if cfg!(feature = "cfi") {
                 CfiCounter::delay();
@@ -162,11 +255,183 @@ cfi_assert_macro!(cfi_assert_lt, <, Ord, PartialOrd, AssertLtFail);
 cfi_assert_macro!(cfi_assert_ge, >=, Ord, PartialOrd, AssertGeFail);
 cfi_assert_macro!(cfi_assert_le, <=, Ord, PartialOrd, AssertLeFail);
 
+// special case for bool assert
+#[inline(always)]
+#[allow(unused)]
+pub fn cfi_assert_bool(cond: bool) {
+    if cfg!(feature = "cfi") {
+        CfiCounter::delay();
+        if !cond {
+            cfi_panic(CfiPanicInfo::AssertEqFail);
+        }
+
+        // Second check for glitch protection
+        CfiCounter::delay();
+        if !cfi_launder(cond) {
+            cfi_panic(CfiPanicInfo::AssertEqFail);
+        }
+    }
+}
+
 #[macro_export]
 macro_rules! cfi_assert {
     ($cond: expr) => {
-        cfi_assert_eq($cond, true)
+        cfi_assert_bool($cond)
     };
+}
+
+#[cfg(not(any(target_arch = "riscv32", target_arch = "riscv64")))]
+pub fn cfi_assert_eq_16_words(a: &[u32; 16], b: &[u32; 16]) {
+    if a != b {
+        cfi_panic(CfiPanicInfo::AssertEqFail)
+    }
+}
+
+/// Unrolled comparison of 16 words
+///
+/// Written in assembly so the trampoline is above the comparisons rather than
+/// below
+#[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
+#[inline(always)]
+pub fn cfi_assert_eq_16_words(a: &[u32; 16], b: &[u32; 16]) {
+    unsafe {
+        core::arch::asm!(
+            "j 3f",
+            "2:",
+            "li a0, 0x01040055",
+            "j cfi_panic_handler",
+            "3:",
+            "lw {tmp0}, 0(a4)",
+            "lw {tmp1}, 0(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 4(a4)",
+            "lw {tmp1}, 4(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 8(a4)",
+            "lw {tmp1}, 8(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 12(a4)",
+            "lw {tmp1}, 12(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 16(a4)",
+            "lw {tmp1}, 16(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 20(a4)",
+            "lw {tmp1}, 20(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 24(a4)",
+            "lw {tmp1}, 24(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 28(a4)",
+            "lw {tmp1}, 28(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 32(a4)",
+            "lw {tmp1}, 32(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 36(a4)",
+            "lw {tmp1}, 36(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 40(a4)",
+            "lw {tmp1}, 40(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 44(a4)",
+            "lw {tmp1}, 44(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 48(a4)",
+            "lw {tmp1}, 48(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 52(a4)",
+            "lw {tmp1}, 52(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 56(a4)",
+            "lw {tmp1}, 56(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            "lw {tmp0}, 60(a4)",
+            "lw {tmp1}, 60(a5)",
+            "bne {tmp0}, {tmp1}, 2b",
+            in("a4") a.as_ptr(),
+            in("a5") b.as_ptr(),
+            tmp0 = out(reg) _,
+            tmp1 = out(reg) _);
+    }
+}
+
+#[cfg(not(any(target_arch = "riscv32", target_arch = "riscv64")))]
+pub fn cfi_assert_ne_16_words(a: &[u32; 16], b: &[u32; 16]) {
+    if a == b {
+        cfi_panic(CfiPanicInfo::AssertNeFail)
+    }
+}
+
+/// Unrolled comparison of 16 words
+///
+/// Written in assembly so the trampoline is above the comparisons rather than
+/// below
+#[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
+#[inline(always)]
+pub fn cfi_assert_ne_16_words(a: &[u32; 16], b: &[u32; 16]) {
+    unsafe {
+        core::arch::asm!(
+            "j 3f",
+            "2:",
+            "li a0, 0x01040056",
+            "j cfi_panic_handler",
+            "3:",
+            "lw {tmp0}, 0(a4)",
+            "lw {tmp1}, 0(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 4(a4)",
+            "lw {tmp1}, 4(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 8(a4)",
+            "lw {tmp1}, 8(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 12(a4)",
+            "lw {tmp1}, 12(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 16(a4)",
+            "lw {tmp1}, 16(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 20(a4)",
+            "lw {tmp1}, 20(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 24(a4)",
+            "lw {tmp1}, 24(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 28(a4)",
+            "lw {tmp1}, 28(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 32(a4)",
+            "lw {tmp1}, 32(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 36(a4)",
+            "lw {tmp1}, 36(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 40(a4)",
+            "lw {tmp1}, 40(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 44(a4)",
+            "lw {tmp1}, 44(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 48(a4)",
+            "lw {tmp1}, 48(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 52(a4)",
+            "lw {tmp1}, 52(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 56(a4)",
+            "lw {tmp1}, 56(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "lw {tmp0}, 60(a4)",
+            "lw {tmp1}, 60(a5)",
+            "bne {tmp0}, {tmp1}, 4f",
+            "j 2b",
+            "4:",
+            in("a4") a.as_ptr(),
+            in("a5") b.as_ptr(),
+            tmp0 = out(reg) _,
+            tmp1 = out(reg) _,);
+    }
 }
 
 #[cfg(not(any(target_arch = "riscv32", target_arch = "riscv64")))]
@@ -187,7 +452,7 @@ pub fn cfi_assert_eq_12_words(a: &[u32; 12], b: &[u32; 12]) {
         core::arch::asm!(
             "j 3f",
             "2:",
-            "li a0, 0x05", // 0x05 corresponds to CfiPanicInfo::AssertEqFail
+            "li a0, 0x01040055",
             "j cfi_panic_handler",
             "3:",
             "lw {tmp0}, 0(a4)",
@@ -251,7 +516,7 @@ pub fn cfi_assert_eq_8_words(a: &[u32; 8], b: &[u32; 8]) {
         core::arch::asm!(
             "j 3f",
             "2:",
-            "li a0, 0x05", // 0x05 corresponds to CfiPanicInfo::AssertEqFail
+            "li a0, 0x01040055",
             "j cfi_panic_handler",
             "3:",
             "lw {tmp0}, 0(a4)",
@@ -303,7 +568,7 @@ pub fn cfi_assert_eq_6_words(a: &[u32; 6], b: &[u32; 6]) {
         core::arch::asm!(
             "j 3f",
             "2:",
-            "li a0, 0x05", // 0x05 corresponds to CfiPanicInfo::AssertEqFail
+            "li a0, 0x01040055",
             "j cfi_panic_handler",
             "3:",
             "lw {tmp0}, 0(a4)",
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -8,8 +8,7 @@ File Name:
 
 --*/
 
-#![cfg_attr(not(feature = "cfi-test"), no_std)]
-
+#![no_std]
 extern crate core;
 
 mod cfi;
@@ -18,38 +17,24 @@ mod xoshiro;
 
 pub use cfi::*;
 pub use cfi_counter::{CfiCounter, CfiInt};
-pub use core::cell::Cell;
 pub use xoshiro::Xoshiro128;
 
 #[repr(C)]
 pub struct CfiState {
-    val: Cell<u32>,
-    mask: Cell<u32>,
+    val: u32,
+    mask: u32,
     prng: Xoshiro128,
 }
 
 #[cfg(feature = "cfi-test")]
-thread_local! {
-    static CFI_STATE: CfiState = CfiState {
-        val: Cell::new(0),
-        mask: Cell::new(0),
-        prng: Xoshiro128::new_unseeded(),
-    };
-}
-
-#[cfg(feature = "cfi-test")]
-fn with_cfi_state<R, F: FnOnce(&CfiState) -> R>(f: F) -> R {
-    CFI_STATE.with(f)
-}
+static mut CFI_STATE: CfiState = CfiState {
+    val: 0,
+    mask: 0,
+    prng: Xoshiro128::new_unseeded(),
+};
 
 #[cfg(not(feature = "cfi-test"))]
 extern "C" {
     #[link_name = "CFI_STATE_ORG"]
-    static CFI_STATE: CfiState;
-}
-
-#[cfg(not(feature = "cfi-test"))]
-fn with_cfi_state<R, F: FnOnce(&CfiState) -> R>(f: F) -> R {
-    // This is only safe because we are in a single-threaded environment (CfiState is !Sync)
-    f(unsafe { &CFI_STATE })
+    static mut CFI_STATE: CfiState;
 }
